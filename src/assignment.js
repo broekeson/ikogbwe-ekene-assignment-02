bd7d6821aaa7cecd858e8981b3365a1facc06bf8
Ekene Ikogbwe <broekeson@gmail.com>
2022-07-07 15:56:24 +0000

    Attempt Challenge-1 and Challenge-2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+    for(let i = 0; i < arrayOfNumbers.length; i++){
+        sum += arrayOfNumbers[i];
+    }
+    
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +33,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function countEvenNumbers  (arrayOfNumbers) {
+    let evenNumbers = [];
+    arrayOfNumbers.forEach(number=>{
+      if(number % 2  === 0){
+        evenNumbers.push(number)
+      }
+    })
+  
+    return evenNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
